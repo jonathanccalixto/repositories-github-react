20c8aef0abb1193866d63c75d865ce0b366c582e
Jonathan C. Calixto <jonathanccalixto@gmail.com>
2019-07-27 19:00:59 -0300

    Adding container styles

--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,6 +1,13 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  background: #fff;
+  border-radius: 4px;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
+  margin: 80px auto;
+  max-width: 700px;
+  padding: 30px;
+`;
 
 export const Form = styled.form``;
 
