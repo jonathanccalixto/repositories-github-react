3f3634579e1aeabb19ac784d254b1c94dc386ed0
Jonathan C. Calixto <jonathanccalixto@gmail.com>
2019-07-27 18:28:16 -0300

    Removing chaining style example

--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,12 +1,5 @@
 import styled from 'styled-components';
 
 export const Title = styled.h1`
-  color: ${props => (props.error ? '#FF170F' : '#7159c1')};
-  font-family: Arial, Helvetica, sans-serif;
-  font-size: 24px;
-
-  small {
-    color: #333;
-    font-size: 14px;
-  }
+  color: #fff;
 `;
