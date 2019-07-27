8165e5b7b06392e90f8c6dda34dd0cae4f9f0068
Jonathan C. Calixto <jonathanccalixto@gmail.com>
2019-07-27 20:19:13 -0300

    Making Main Pages a Stateful Component

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,24 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import { FaGithubAlt, FaPlus } from 'react-icons/fa';
 
 import { Container, Form, SubmitButton } from './styles';
 
-export default function Main() {
-  return (
-    <Container>
-      <h1>
-        <FaGithubAlt />
-        Repositórios
-      </h1>
+export default class Main extends Component {
+  state = {
+    newRepo: '',
+  };
 
-      <Form onSubmit={() => {}}>
-        <input type="text" placeholder="Adicionar repositório" />
+  handleInputChange = e => {
+    this.setState({ newRepo: e.target.value });
+  };
 
-        <SubmitButton>
-          <FaPlus color="#FFF" size={14} />
-        </SubmitButton>
-      </Form>
-    </Container>
-  );
+  render() {
+    const { newRepo } = this.state;
+
+    return (
+      <Container>
+        <h1>
+          <FaGithubAlt />
+          Repositórios
+        </h1>
+
+        <Form onSubmit={() => {}}>
+          <input
+            type="text"
+            placeholder="Adicionar repositório"
+            value={newRepo}
+            onChange={this.handleInputChange}
+          />
+
+          <SubmitButton>
+            <FaPlus color="#FFF" size={14} />
+          </SubmitButton>
+        </Form>
+      </Container>
+    );
+  }
 }
