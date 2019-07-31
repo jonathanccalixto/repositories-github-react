d5200fef4bf193e76e2fd2108b9136f1f1e03909
Jonathan C. Calixto <jonathanccalixto@gmail.com>
2019-07-31 14:01:54 -0300

    Adding Back link in Repository page

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 import api from '../../services/api';
@@ -53,6 +54,7 @@ export default class Repository extends Component {
     return (
       <Container>
         <Owner>
+          <Link to="/">Voltar aos repositórios</Link>
           <img src={repository.owner.avatar_url} alt={repository.owner.login} />
           <h1>{repository.name}</h1>
           <p>{repository.description}</p>
