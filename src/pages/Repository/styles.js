22aeff2880a2e8111bbf212867fa2adcf383157c
Jonathan C. Calixto <jonathanccalixto@gmail.com>
2019-07-31 12:01:09 -0300

    Styling the Loading Component

--- src/pages/Repository/styles.js
+++ src/pages/Repository/styles.js
@@ -1,3 +1,11 @@
 import styled from 'styled-components';
 
-export const Loading = styled.div``;
+export const Loading = styled.div`
+  align-items: center;
+  color: #fff;
+  display: flex;
+  font-size: 30px;
+  font-weight: bold;
+  height: 100vh;
+  justify-content: center;
+`;
